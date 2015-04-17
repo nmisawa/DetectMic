7c4149d3edacb2ec9923046928fbcedf2fb81707
Noriaki Misawa <n.misawa@freebit.net>
2015-04-17 18:18:01 +0900

    Initial Commit

---
+++ DetectMic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DetectMic
+//
+//  Created by Noriaki Misawa on 2015/04/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DetectMicTests/DetectMicTests.swift
@@ -0,0 +1,36 @@
+//
+//  DetectMicTests.swift
+//  DetectMicTests
+//
+//  Created by Noriaki Misawa on 2015/04/17.
+//
+//
+
+import UIKit
+import XCTest
+
+class DetectMicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
